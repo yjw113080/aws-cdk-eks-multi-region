c0fdb0c3646cab1e411928118eb375dc31dd06e8
Yeom <jiwony@a483e7428246.ant.amazon.com>
2020-04-01 14:52:37 +0900

    undefined handled in read-file module

--- utils/read-file.ts
+++ utils/read-file.ts
@@ -5,21 +5,25 @@ import * as eks from '@aws-cdk/aws-eks';
 
 export function readYamlFromDir(dir: string, cluster: eks.Cluster) {
     fs.readdir(dir, 'utf8', (err, files) => {
+      if (files!=undefined) {
         files.forEach((file) => {
-          if (file.split('.').pop()=='yaml') {
-            fs.readFile(dir+file, 'utf8', (err, data) => {
-              if (data!=undefined) {
-                // data.split('{{region_name}}').join(region);
-                // data.split('{{cluster_name}}').join(cluster.clusterName);
-                let i = 0;
-      
-                yaml.loadAll(data).forEach((item) => {
-                  cluster.addResource(file.substr(0,file.length-5)+i, item);
-                  i++;
-                })
-              }
-            })
-          }
-        })
+        if (file.split('.').pop()=='yaml') {
+          fs.readFile(dir+file, 'utf8', (err, data) => {
+            if (data!=undefined) {
+              // data.split('{{region_name}}').join(region);
+              // data.split('{{cluster_name}}').join(cluster.clusterName);
+              let i = 0;
+    
+              yaml.loadAll(data).forEach((item) => {
+                cluster.addResource(file.substr(0,file.length-5)+i, item);
+                i++;
+              })
+            }
+          })
+        }
+      })} else {
+        console.log(`${dir} is empty`);
+      }
+        
       })
 }
