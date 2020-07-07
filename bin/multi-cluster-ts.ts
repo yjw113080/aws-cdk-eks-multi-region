b4f1f7a75445824e7f6a80a5dacfcbddb936853c
Jiwon Yeom <jiwony@amazon.com>
2020-07-07 20:58:56 +0900

    updated k8s cluster version and package.json

--- bin/multi-cluster-ts.ts
+++ bin/multi-cluster-ts.ts
@@ -9,7 +9,7 @@ import { CicdStack } from '../lib/cicd-stack';
 const app = new cdk.App();
 
 const account = app.node.tryGetContext('account') || process.env.CDK_INTEG_ACCOUNT || process.env.CDK_DEFAULT_ACCOUNT;
-const primaryRegion = {account: account, region: 'us-east-1'};
+const primaryRegion = {account: account, region: 'ap-northeast-1'};
 const secondaryRegion = {account: account, region: 'us-west-2'};
 
 const primaryCluster = new ClusterStack(app, `ClusterStack-${primaryRegion.region}`, {env: primaryRegion })
